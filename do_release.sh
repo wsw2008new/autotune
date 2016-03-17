62ebbb6ea084c3f9536ee555bc35136188155473
Ryan Mark <ryan@mrk.cc>
2016-03-17 12:17:22 -0400

    update release script

--- do_release.sh
+++ do_release.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+if ! git diff-index --quiet HEAD --; then
+  echo You must commit all your changes before updating the version
+  exit 1
+fi
+
 if [ $# -ne 1 ]; then
-    echo $0: usage: do_release.sh version
-    exit 1
+  old_version=$(jq '.version' package.json)
+  echo Current version is $old_version. Please enter a new version:
+  read $version
+else
+  version=$1
 fi
 
 command -v jq >/dev/null 2>&1 || { echo >&2 "Missing jq. Please install jq."; exit 1; }
 command -v sponge >/dev/null 2>&1 || { echo >&2 "Missing sponge. Please install moreutils."; exit 1; }
 
-if git diff-index --quiet HEAD --; then
-    # no changes
-  jq ".version |= $1" package.json |sponge package.json
-  git commit -am "bump to $1"
-  git tag $1
-  git push --tags
-else
-  echo You must commit all your changes before updating the version
-    # changes
-fi
+jq ".version |= $version" package.json |sponge package.json
+git commit -am "bump to $version"
+git tag v$version
+#git push --tags
 
