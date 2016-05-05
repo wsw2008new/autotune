f0c6b64c1f9c34057eb638fc8c72139845ea4a53
caseymm <casey.miller@voxmedia.com>
2016-05-05 14:25:36 -0400

    trying to get stuff to work on retina screenshots

--- lib/work_dir/phantom.rb
+++ lib/work_dir/phantom.rb
@@ -7,7 +7,31 @@ module WorkDir
       return unless phantomjs?
       working_dir do
         script_path = File.expand_path('../screenshot.js', __FILE__).to_s
+        script_path2 = File.expand_path('../screenshot2.js', __FILE__).to_s
         phantomjs script_path, url
+        phantomjs script_path2, url
+
+        pixel_ratio_path = File.expand_path('../pixelratio.js', __FILE__).to_s
+        # phantomjs pixel_ratio_path, url, '~/Desktop/testImg.jpg', '2'
+        # screenshots = [
+        #   {:dimensions => [970,300],
+        #     :filename => './screenshots/large.png'},
+        #   {:dimensions => [720,300],
+        #     :filename => './screenshots/medium.png'},
+        #   {:dimensions => [400,200],
+        #     :filename => './screenshots/small.png'}
+        # ];
+        screenshots = [
+          [ ['970','300'], './screenshots/large.png'],
+          [ ['720','300'], './screenshots/medium.png'],
+          [ ['400','200'], './screenshots/small.png']
+        ];
+
+        screenshots.each do |image|
+          phantomjs pixel_ratio_path, url, image[1], image[0][0], image[0][1], '2'
+          #  phantomjs pixelratio.js [url] [file] [pixelRatio]
+        end
+
       end
     end
 
@@ -28,5 +52,12 @@ module WorkDir
     def phantomjs(*args)
       cmd(*['phantomjs', '--disk-cache=true'] + args)
     end
+    #
+    # def phantomPixel(*args)
+    #   puts '@@@'
+    #   puts args
+    #   pixel_ratio_path = File.expand_path('../pixelratio.js', __FILE__).to_s
+    #   cmd(*['phantomjs '+pixel_ratio_path, '--disk-cache=true'] + args)
+    # end
   end
 end
